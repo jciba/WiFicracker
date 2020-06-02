30304a191d1baddafc3c18267ac87bfb310abcca
jciba <jjciba@gmail.com>
2020-06-02 15:37:05 +0100

    changing from Popen to run

--- start.py
+++ start.py
@@ -14,28 +14,30 @@ def info():
 def interface_check():
     global interface
     global eg
-    #interface = input("Please enter the name of the wireless interface: ")
-    proc = Popen(["iw", "dev"], stdout=PIPE, stderr=PIPE)
-    for line in proc.stdout:
-        ln = line.decode('utf8').split()
-        if(ln[0]=='Interface'):
-            interface.append(ln)
-    proc.wait()
-    proc.kill()
-
+    
+    #proc = Popen(["sudo","iw", "dev"], stdout=PIPE, stderr=PIPE)
+    proc = subprocess.run(["sudo","iw", "dev"],capture_output=True,text=True)
+    w = proc.stdout.split()
+    x = len(w)    
+    for i in range(0,x):
+        if (w[i] == "Interface"):
+            interface.append(w[i+1])
+            #print(w[i+1])
+ 
 def interface_select():
     global interface
     global nic
+    global monNic
     print("Availiable interfaces \n")
     x = 0
     for line in interface:
-        print(str(x) + " "+ line[1])
+        print(str(x) + " "+ line)
         x += 1
 
     w = int(input("Please enter ID of a wireless interface you wish to use: "))
     print("\n")
 
-    nic = interface[w][1]
+    nic = interface[w]
 
     print("Using: " + nic)
 
@@ -45,37 +47,51 @@ def get_ssid():
     for line in proc.stdout:
         print(line)
 
+def check_processes():
+    global nic
+    global monNic
+    proc = subprocess.run(["sudo","airmon-ng", "check",],capture_output=True,text=True)
+    print(proc.stdout)
+
 def start_monitor():
     global nic
     global monNic
-    proc = Popen(["sudo","-i","airmon-ng", "check","kill"],stdout=PIPE, stderr=PIPE)
-    proc.wait()
+    print("Attempting to stop all processes that may cause issues")
+    #proc = Popen(["sudo","airmon-ng", "check","kill"],stdout=PIPE, stderr=PIPE)
+    #proc.communicate(timeout=20)
+    proc = subprocess.run(["sudo","airmon-ng", "check","kill"],capture_output=True,text=True)
     for line in proc.stdout:
-        print(line)
-    proc.wait()
-    proc.kill()
+        print(line)    
+    
+    print("Processes terminated")
+
+    print("Starting monitor mode")
     proc = Popen(["sudo","-i","airmon-ng", "start",nic],stdout=PIPE, stderr=PIPE)
-    proc.wait()
+    
+    #proc.communicate(timeout=20)
     for line in proc.stdout:
         ln = line.decode('utf8').split()
         for x in ln:
             print(x)
 
-    proc.kill()
+    
     monNic = nic + "mon"
 
 def stop_monitor():
     global nic
     global monNic
+    proc = subprocess.run(["sudo","airmon-ng", "stop",monNic],capture_output=True,text=True)
+    print("Stopping monitor mode")
     
-    print(monNic)
+    
 
 
-info()
+#info()
 interface_check()
 interface_select()
-start_monitor()
+check_processes()
+#start_monitor()
 
 #get_ssid()
 
-stop_monitor()
+#stop_monitor()
